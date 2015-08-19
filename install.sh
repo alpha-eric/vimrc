8a7b27f24111ca58a1a945a355d677afa5048f3a
eric hsieh <eric_hsieh@alphanetworks.com>
2015-08-19 15:06:02 +0800

    fix pwd bug.

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-PWD=$(pwd)
-ln -sf $(PWD)/.vimrc ~/.vimrc
-ln -sf $(PWD)/.vim ~/.vim
+ln -sf $(pwd)/.vimrc ~/.vimrc
+ln -sf $(pwd)/.vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc +BundleInstall! +BundleClean +qall
 sh ~/.vim/bundle/YouCompleteMe/install.sh
