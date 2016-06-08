f7e420e8b0d3a98082cdf09fded4b2ae12ed614c
alpha-eric <eric_hsieh@alphanetworks.com>
2016-06-08 10:35:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.vim ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc +BundleInstall! +BundleClean +qall
-sh ~/.vim/bundle/YouCompleteMe/install.sh
+sh ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
