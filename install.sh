f3991fddc6687bfd602cbbe7e38c917280b41e64
eric hsieh <eric_hsieh@alphanetworks.com>
2016-09-19 14:50:25 +0800

    add .ssh folder.

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ ln -sf $(pwd)/.vimrc ~/.vimrc
 ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.inputrc ~/.inputrc
 ln -sf $(pwd)/.bash_profile ~/.bash_profile
-ln -sf $(pwd)/..kermrc ~/.kermrc
+ln -sf $(pwd)/.kermrc ~/.kermrc
+ln -sf $(pwd)/.ssh ~/.ssh
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc +BundleInstall! +BundleClean +qall
 sh ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
