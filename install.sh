964648fbaec92c638c7c3f3d69d15fbbdd4678eb
alpha-eric <eric_hsieh@alphanetworks.com>
2017-01-12 10:39:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf $(pwd)/.inputrc ~/.inputrc
 ln -sf $(pwd)/.bash_profile ~/.bash_profile
 ln -sf $(pwd)/.kermrc ~/.kermrc
 ln -sf $(pwd)/.ssh ~/.ssh
+ln -sf $(pwd)/.gitconfig ~/.gitconfig
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc +BundleInstall! +BundleClean +qall
 sh ~/.vim/bundle/YouCompleteMe/install.sh
