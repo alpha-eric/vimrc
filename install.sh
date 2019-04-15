c50280d2fba901ce9f8d04a6ef80d4eefcf02796
eric hsieh <eric_hsieh@alphanetworks.com>
2019-04-15 09:24:41 +0800

    add h264/opus extrator from rtp tool

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 ln -sf $(pwd)/.vimrc ~/.vimrc
-ln -sf $(pwd)/.vim ~/.vim
 ln -sf $(pwd)/.inputrc ~/.inputrc
 ln -sf $(pwd)/.bash_profile ~/.bash_profile
 ln -sf $(pwd)/.kermrc ~/.kermrc
-ln -sf $(pwd)/.ssh ~/.ssh
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
+# folder
+ln -sf $(pwd)/.vim ~/
+ln -sf $(pwd)/.ssh ~/
+ln -sf $(pwd)/.config ~/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -u ~/.vimrc +BundleInstall! +BundleClean +qall
 # for mac
